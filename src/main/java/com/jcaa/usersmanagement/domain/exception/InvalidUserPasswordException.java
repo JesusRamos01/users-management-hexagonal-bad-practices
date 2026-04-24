e24cf145c43c334a99356237da3554fe11048566
JesusRamos01 <149116691+JesusRamos01@users.noreply.github.com>
2026-04-24 15:59:32 -0500

    refactor: corrige regla 10 reemplazando texto hardcodeado por constante

--- src/main/java/com/jcaa/usersmanagement/domain/exception/InvalidUserPasswordException.java
+++ src/main/java/com/jcaa/usersmanagement/domain/exception/InvalidUserPasswordException.java
@@ -2,16 +2,20 @@ package com.jcaa.usersmanagement.domain.exception;
 
 public final class InvalidUserPasswordException extends DomainException {
 
+  private static final String INVALID_USER_PASSWORD_MESSAGE = "The user password is not valid.";
+  private static final String INVALID_USER_PASSWORD_MIN_LENGTH_MESSAGE =
+      "The user password must have at least %d characters.";
+
   private InvalidUserPasswordException(final String message) {
     super(message);
   }
 
   public static InvalidUserPasswordException becauseValueIsEmpty() {
-    return new InvalidUserPasswordException("The user password must not be empty.");
+    return new InvalidUserPasswordException(INVALID_USER_PASSWORD_MESSAGE);
   }
 
   public static InvalidUserPasswordException becauseLengthIsTooShort(final int minimumLength) {
     return new InvalidUserPasswordException(
-        String.format("The user password must have at least %d characters.", minimumLength));
+        String.format(INVALID_USER_PASSWORD_MIN_LENGTH_MESSAGE, minimumLength));
   }
 }
